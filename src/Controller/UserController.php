a34ead0756625a0499d3851f436ff6b273bc41aa
K1ngDavid <david.roufe@gmail.com>
2022-09-17 23:23:49 +0200

    Optimisation du code et ajout de commentaires

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,24 +13,24 @@ class UserController implements \Quizz\Core\Controller\ControllerInterface
     private $error = null;
     private $validate = false;
 
-    public function inputRequest(array $tabInput)
+    public function inputRequest(array $tabInput) // c'est ici qu'on va gérer l'input du user
     {
         // TODO: Implement inputRequest() method.
-        if($_POST){
-            $test = FormControl::controlCheck($_POST);
-            $secure = FormControl::securityCheck($_POST['password']);
-            if($test == null){
-                if(is_string($secure)){
+        if($_POST){ // vérifie si des variables http en méthode POST existent
+            $test = FormControl::controlCheck($_POST); // on fait des vérifications sur les champs
+            $secure = FormControl::securityCheck($_POST['password']); // sécurisation du mdp
+            if($test == null){ // si la variable est de type null il n'y a alors aucune erreur
+                if(is_string($secure)){ // si la variable est de type string alors on peut inséreur l'étudiant
                     $this->validate = true;
-                    $form = new FormControl($_POST['login'] , $secure ,$_POST['lastname'],$_POST['firstname'],$_POST['email']);
+                    $form = new FormControl($_POST['login'] , $secure ,$_POST['lastname'],$_POST['firstname'],$_POST['email']); // création d'un objet de type form
                     $user = new UserModel();
-                    $user->addUser($form->getLogin(),$form->getMdp(),$form->getNom(),$form->getPrenom(),$form->getEmail());
+                    $user->addUser($form->getLogin(),$form->getMdp(),$form->getNom(),$form->getPrenom(),$form->getEmail()); // ajout dans la base de donnée
                 }else $this->error = $secure->getMessage();
             }else $this->error = $test;
         }
     }
 
-    public function outputEvent()
+    public function outputEvent() // c'est ici qu'on va output le résultat :)
     {
         if($this->error){
             return TwigCore::getEnvironment()->render(
