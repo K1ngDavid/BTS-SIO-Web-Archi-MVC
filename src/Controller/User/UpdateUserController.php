4bd8ac0d70d64b6f2f4a10a5d0705d2df234df33
K1ngDavid <david.roufe@gmail.com>
2022-09-19 23:52:02 +0200

    Optimisation du code et ajout de commentaires

--- src/Controller/User/UpdateUserController.php
+++ src/Controller/User/UpdateUserController.php
@@ -20,25 +20,25 @@ class UpdateUserController implements \Quizz\Core\Controller\ControllerInterface
         // TODO: Implement inputRequest() method.
         $unEtudiant = new EtudiantModel();
         $this->id = $tabInput['VARS']['id'];
+        $flag = 0;
         $this->etudiant = $unEtudiant->getFetchIdEtudiant($this->id);
-        echo $this->etudiant->getEmail();
         if($_POST){
             $secure = FormControl::securityCheck($_POST['password']); // sécurisation du mdp
-            if($this->etudiant->getEmail() == $_POST['email']){
-                if($this->etudiant->getLogin() == $_POST['login']){
+            if($this->etudiant->getEmail() == $_POST['email']){ // si l'ancien mdp est égal au nouveau
+                if($this->etudiant->getLogin() == $_POST['login']){ // si l'ancien login est égal au nouveau
                     $test = FormControl::updateControlCheck($_POST,null,null); // on fait des vérifications sur les champs
                 }else{
-                    $test  = FormControl::updateControlCheck($_POST,$_POST['login'],null);
+                    $test  = FormControl::updateControlCheck($_POST,$_POST['login'],null); // si le login est différent alors on le modifie
                 }
-            }else{
-                $test = FormControl::updateControlCheck($_POST, null ,$_POST['email']);
+            }else{// si l'email est différent alors on le modifie
+                $flag = null;
                 if($this->etudiant->getLogin() != $_POST['login']) {
-                    $test = FormControl::updateControlCheck($_POST, $_POST['login'], $_POST['email']);
+                    $flag = $_POST['login'];
                 }
+                $test = FormControl::updateControlCheck($_POST, $flag ,$_POST['email']);
             }
             if($test == null) {
                 if (is_string($secure)) {
-
                     $unEtudiant->updateEtudiant($tabInput['VARS']['id'], $_POST['login'], $secure, $_POST['lastname'], $_POST['firstname'], $_POST['email']); // on update les changements
                     $this->validate = true;
                 }else $this->error = $secure->getMessage(); // si erreur, on la sauvegarde pour l'afficher avec le message correspondant
@@ -54,7 +54,8 @@ class UpdateUserController implements \Quizz\Core\Controller\ControllerInterface
             return TwigCore::getEnvironment()->render(
                 'user/updateUser.html.twig',[
                     'error' => true,
-                    'causeError' =>$this->error
+                    'causeError' =>$this->error,
+                    'etudiant' => $this->etudiant
                 ]
             );
         }
@@ -69,14 +70,11 @@ class UpdateUserController implements \Quizz\Core\Controller\ControllerInterface
             );
         }
         else{
-            $this->password = $_POST['password'];
             return TwigCore::getEnvironment()->render(
                 'user/updateUser.html.twig',[
                     'etudiant' => $this->etudiant,
-                    'password' => $this->password
                 ]
             );
         }
-    header("Refresh:0");
     }
 }
