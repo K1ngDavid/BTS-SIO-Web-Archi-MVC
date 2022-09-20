6479ca0f8e5bc059d532b125801282e4a78f9606
K1ngDavid <david.roufe@gmail.com>
2022-09-20 21:44:31 +0200

    Optimisation du code et ajout de commentaires

--- src/Controller/User/AddUserController.php
+++ src/Controller/User/AddUserController.php
@@ -17,7 +17,6 @@ class AddUserController implements \Quizz\Core\Controller\ControllerInterface
         if($_POST){ // vérifie si des variables http en méthode POST existent
             $test = FormControl::controlCheck($_POST); // on fait des vérifications sur les champs
             $secure = FormControl::securityCheck($_POST['password']); // sécurisation du mdp
-            echo $_POST['password'];
             if($test == null){ // si la variable est de type null il n'y a alors aucune erreur
                 if(is_string($secure)){ // si la variable est de type string alors on peut insérer l'étudiant
                     $this->validate = true;
